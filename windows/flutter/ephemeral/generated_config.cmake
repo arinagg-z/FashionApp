90ce82a4d6e4b875d8061ffcef1a2474c40103de
arinagg-z <117895829+arinagg-z@users.noreply.github.com>
2023-08-27 17:11:32 +0530

    first commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,25 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Program Files\\Android\\Android Studio\\Prerequistes\\flutter_windows_3.7.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Sanjay\\Documents\\flutter\\fashionapp" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Program Files\\Android\\Android Studio\\Prerequistes\\flutter_windows_3.7.3-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Sanjay\\Documents\\flutter\\fashionapp"
+  "FLUTTER_ROOT=C:\\Program Files\\Android\\Android Studio\\Prerequistes\\flutter_windows_3.7.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Sanjay\\Documents\\flutter\\fashionapp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Sanjay\\Documents\\flutter\\fashionapp"
+  "FLUTTER_TARGET=C:\\Users\\Sanjay\\Documents\\flutter\\fashionapp\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Sanjay\\Documents\\flutter\\fashionapp\\.dart_tool\\package_config.json"
+)
